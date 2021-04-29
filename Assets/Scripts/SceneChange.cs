4590dd7480a1d253c0779c7d4171ea031489241c
Sanzhar <sanzh98@gmail.com>
2021-04-30 02:36:53 +0800

    Changes to screen changer

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
@@ -9,31 +10,29 @@ public class SceneChange : MonoBehaviour
 
     [SerializeField] private string sceneName;
     public int[] counts;
-    public GameObject[] elements;
-    // private Dictionary<GameObject>
-    Dictionary<string, int> requirements = new Dictionary<string, int>();
+    public TextMeshProUGUI[] counters;
+
+    public TextMeshProUGUI[] requirementDisplayers;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (counts.Length != elements.Length)
+        if (counts.Length != counters.Length || counts.Length != requirementDisplayers.Length)
             throw new System.Exception("Requirement and count lengths should be the same");
         for (int i = 0; i < counts.Length; ++i)
         {
-            if (requirements.ContainsKey(elements[i].name))
-                throw new System.Exception("Elements should be unique");
-            requirements[elements[i].name] = counts[i];
+            requirementDisplayers[i].text = counts[i].ToString();
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
-            GameObject[] collectibleCounts = GameObject.FindGameObjectsWithTag("CollectibleCount");
-            bool satisfied = true;
-            foreach (GameObject collectibleCount in collectibleCounts) {
-                if (requirements[collectibleCount.GetComponent<CollectibleCount>().element.name] > int.Parse(collectibleCount.GetComponent<UnityEngine.UI.Text>().text)) {
+            var satisfied = true;
+            for (int i = 0; i < counts.Length; ++i) {
+                if (counts[i] > int.Parse(counters[i].text))
+                {
                     satisfied = false;
                 }
             }
